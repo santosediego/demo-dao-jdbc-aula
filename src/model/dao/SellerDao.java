1a1234ae7457cffab6ba46d16e14b8f265c20a83
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-03-01 15:53:56 -0300

    findById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -6,7 +6,7 @@ import model.entities.Seller;
 
 public interface SellerDao {
 
-	void isnert(Seller obj);
+	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Seller obj);
 	Seller findById(Integer id);
