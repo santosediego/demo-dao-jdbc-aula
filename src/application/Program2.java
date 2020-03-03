7016ddfa11ade626d262651ac3413dec47cfe7a2
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-03-03 05:47:28 -0300

    DepartmentDao implementation and test

---
+++ src/application/Program2.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.util.List;
+import java.util.Scanner;
+
+import db.DB;
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.impl.DepartmentDaoJDBC;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("-- Test 1: department findById --");
+		Department department = departmentDao.findById(2);
+		System.out.println(department);
+		
+		System.out.println("\n-- Test 2: department findAll --");
+		List<Department> list = departmentDao.findAll();
+		for(Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n-- Test 3: department insert --");
+		Department newDepartment = new Department(null, "Música");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted!\nID: " + newDepartment.getId());
+		
+		System.out.println("\n-- Test 3: department insert --");
+		department = departmentDao.findById(1);
+		department.setName("Tools");
+		departmentDao.update(department);
+		System.out.println("Update completed!");
+		
+		System.out.println("\n-- Test 4: department deleteById --");
+		System.out.print("Enter id for delete register: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delele completed!");
+
+		sc.close();
+		DB.closeConnection();
+		System.out.println("\nTudo posso Naquele que me Fortalece!!");
+	}
+}
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -3,13 +3,13 @@ package model.dao;
 import java.util.List;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public interface DepartmentDao {
 
 	void insert(Department obj);
 	void update(Department obj);
-	void deleteById(Department obj);
+	void deleteById(Integer id);
 	Department findById(Integer id);
 	List<Department> findAll();
-	
 }
